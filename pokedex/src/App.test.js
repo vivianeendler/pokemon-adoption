01a8e04f361497590365f855e8d68d921175b06f
Viviane Dilkin Endler <vdilkin.external@axa-assistance.us>
2023-11-23 17:22:03 -0300

    change test file

--- pokedex/src/App.test.js
+++ pokedex/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Pokemon Api link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Pokemon Api/i);
   expect(linkElement).toBeInTheDocument();
 });
